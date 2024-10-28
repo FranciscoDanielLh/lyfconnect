9fbcea2796f773234f5912e2e6347ef0d20a9ade
franc <franc@LAPTOP-EGV30K89>
2024-10-29 06:59:12 +0800

    Final deploy with data

--- Frontend/.next/static/chunks/webpack.js
+++ Frontend/.next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e44bd3d3144173e5"; }
+/******/ 		__webpack_require__.h = function() { return "f52b2a88b30402be"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
